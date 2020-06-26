78ddb9e240bdec43f1d0b86ce0cd722638aee453
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-27 01:50:00 +0800

    api routes + get request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,5 +2,15 @@ const express = require('express')
 const router = express.Router();
 const db = require('../models');
 
+router.get('/api/workouts', function(req,res){
+db.Workout.find({})
+.then((data) => {
+    console.log(data)
+    res.json(data)
+})
+.catch((error) => {
+    res.json(error)
+})
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 //NPM Packages
-const express = require('express');
-const chalk = require('chalk')
-const mongoose = require('mongoose')
+const express = require("express");
 const app = express();
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-//Mongoose
-const logger = require('morgan');
-app.use(logger('dev'));
-
-//Express Middleware
+//Middleware
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //PORT
 const PORT = process.env.PORT || 8080;
 
 //Routes
-app.use(require('./routes/html-routes'))
+app.use(require("./routes/html-routes"));
+app.use(require("./routes/api-routes"));
 
 //Listener
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 mongoose.connection
-.once('open', () => console.log(chalk.green.bold('Connected to Mongoose')))
-.on('error', error => {
-    console.log(chalk.red('Your Error: ', error))
-})
-app.listen(PORT, function() {
-  console.log(chalk.blue.bold('Access Granted || PORT: ') + chalk.yellow.bold(PORT));
+  .once("open", () => console.log(chalk.green.bold("Connected to Mongoose")))
+  .on("error", (error) => {
+    console.log(chalk.red("Your Error: ", error));
   });
+app.listen(PORT, function () {
+  console.log(
+    chalk.blue.bold("Access Granted || PORT: ") + chalk.yellow.bold(PORT)
+  );
+});
