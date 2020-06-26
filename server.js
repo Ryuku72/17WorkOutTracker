8a39c0a13603fd7363a12870eae73b78e9a35b60
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-26 19:16:05 +0800

    express routes done

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-//if deployed, use the deployed databse otherwide use the local monoHeadlines database
-const MONGODB_URI = process.env.MONGODB_URI || "mongo://localhost/mongoHealines";
+const express = require('express');
+const chalk = require('chalk')
 
-//connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+// const MONGODB_URI = process.env.MONGODB_URI || "mongo://localhost/workout";
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Routes
+require('./routes/htmlroutes')(app)
+
+// //connect to the Mongo DB
+// mongoose.connect(MONGODB_URI);
+
+//listener
+app.listen(PORT, function() {
+  console.log(chalk.blue.bold("Access Point || PORT: ") + chalk.yellow.bold(PORT));
+  });
