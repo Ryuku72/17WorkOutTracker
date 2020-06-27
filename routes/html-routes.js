61dab3428b0b539c32dcf9ccdd7284b7e1b374c5
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-27 22:26:37 +0800

    html routes updated to handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '../public/mock/index.html'));
+    // res.sendFile(path.join(__dirname, '../public/mock/index.html'));
+    res.render("index")
 })
 
 router.get('/exercise', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/mock/exercise.html'));
+   // res.sendFile(path.join(__dirname, '../public/mock/exercise.html'));
+   res.render("exercise")
 });
 
 router.get('/stats', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/mock/stats.html'));
+   // res.sendFile(path.join(__dirname, '../public/mock/stats.html'));
+   res.render("stats")
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //NPM Packages
 const express = require("express");
-const app = express();
 const chalk = require("chalk");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const exphbs = require("express-handlebars");
+const app = express();
 
 //Middleware
 app.use(logger("dev"));
@@ -20,7 +20,6 @@ app.use(require("./routes/api-routes"));
 // Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 
 //Listener
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
